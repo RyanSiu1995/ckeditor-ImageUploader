7ab30002d3037c63c1e81571b561400f5744ca19
Ryan, Siu Long Wa <ryansiu@uw.edu>
2017-05-15 11:10:43 +0800

    First Commit

---
+++ plugin.js
@@ -0,0 +1,29 @@
+CKEDITOR.plugins.add( 'imageUploader', {
+    init: function( editor ) {
+        var fileDialog = $('<input type="file">');
+        
+        fileDialog.on('change', function (e) {
+            var fileTools = CKEDITOR.fileTools, uploadUrl = fileTools.getUploadUrl( editor.config, 'image' );
+            var loader = editor.uploadRepository.create(e.target.files[0]);
+            
+            loader.upload(uploadUrl);
+            loader.on('uploaded', function(evt) {
+                var ele = editor.document.createElement('img');
+                ele.setAttribute('src', evt.sender.url);
+                editor.insertElement(ele);
+                fileDialog[0].value = "";
+            })
+            fileTools.bindNotifications(editor, loader);
+        })
+        editor.ui.addButton( 'Image', {
+            label: 'Insert Image',
+            command: 'openDialog',
+            toolbar: 'insert'
+        });
+        editor.addCommand('openDialog', {
+            exec: function(editor) {
+                fileDialog.click();
+            }
+        })
+    }
+});
